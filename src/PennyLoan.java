dc3f1220598e197ffe400790b63d6f3bf431135e
Gavnukas <psylr3@exmail.nottingham.ac.uk>
2015-11-23 16:15:41 +0000

    hello

--- src/PennyLoan.java
+++ src/PennyLoan.java
@@ -1,8 +1,9 @@
 public class PennyLoan
 {
+  import java.text.DecimalFormat;
         private int amount ; // Amount of loan in pence
         private int length ; // length of subscription in months
-
+        private static DecimalFormat df2 = new DecimalFormat(".##");
         public PennyLoan(int a, int l) {
                 amount = a/100 ;
                 length = l ;
@@ -17,7 +18,7 @@ public class PennyLoan
 
         public double paymentPerMonth() {
                 double r = (double) amount/ (double) length;
-                r = Math.round(r,2);
-                return r;
+
+                return df2.format(r);
         }
 }
